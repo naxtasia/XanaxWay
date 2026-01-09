c212bc3a65816c5d9f3cf06b0c470532d7f7d4e2
Lyricaizm <utkutuzcuu@gmail.com>
2026-01-09 09:34:15 +0300

    Update __init__.py

--- lyricalabs/__init__.py
+++ lyricalabs/__init__.py
@@ -1,1 +1,7 @@
-from .client import NexaClient
+from .client import NexaClient, create_client
+
+__version__ = "0.3.0"
+__author__ = "Lyrica Labs"
+__email__ = "lyricalabs@gmail.com"
+
+__all__ = ["NexaClient", "create_client"]
